3ee104c55eea7b8271bad92f2dde745c93f38e1d
viewjom <syspa@mail.ru>
2022-12-30 02:51:21 +0300

    first commit

---
+++ L24-WC/src/main/java/ru/otus/model/Address.java
@@ -0,0 +1,62 @@
+package ru.otus.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    public Address() {
+    }
+
+    public Address(Long id, String street) {
+        this.id = id;
+        this.street = street;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Address)) return false;
+        Address that = (Address) o;
+        return id == that.id && Objects.equals(street, that.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, street);
+    }
+}
