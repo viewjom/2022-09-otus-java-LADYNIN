2934076f25a1c558c79240c8d8a34354d2c22ee7
viewjom <syspa@mail.ru>
2023-02-18 10:16:10 +0300

    first commit

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,12 +1,11 @@
 public interface Versions {
-
-String guava = "31.0.1-jre";
+    String guava = "31.1-jre";
     String jmh = "1.35";
     String asm = "9.2";
     String glassfishJson = "2.0.1";
 
     String lombok = "1.18.24";
-    String gson = "2.9.1";
+    String gson = "2.10.1";
 
     String mongodb = "4.7.1";
     String mongodbReactive = "4.7.1";
@@ -17,7 +16,7 @@ String guava = "31.0.1-jre";
     String jetty = "11.0.7";
     String freemarker = "2.3.31";
 
-
+    String reflections = "0.10.2";
 
     String sockjs = "1.5.1";
     String stomp = "2.3.4";
@@ -25,9 +24,10 @@ String guava = "31.0.1-jre";
     String springDocOpenapiUi = "2.0.2";
     String jsr305 = "3.0.2";
 
-
     String errorProneAnnotations = "2.11.0";
     String tomcatAnnotationsApi = "6.0.53";
     String grpc = "1.44.1";
+    String wiremock = "3.0.0-beta-2";
+    String r2dbcPostgresql = "0.8.13.RELEASE";
 }
 
